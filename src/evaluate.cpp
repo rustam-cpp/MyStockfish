b7367f1fc1c406d5a7bf7d97804307e4de93f8ad
rustam-cpp <rustam-python@mail.ru>
2025-10-16 19:01:51 +0300

    use simple eval if the position is completely winning

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,7 +46,9 @@ int Eval::simple_eval(const Position& pos) {
          + (pos.non_pawn_material(c) - pos.non_pawn_material(~c));
 }
 
-bool Eval::use_smallnet(const Position& pos) { return std::abs(simple_eval(pos)) > 962; }
+bool Eval::use_smallnet(int Eval) { return std::abs(Eval) > 962; }
+
+bool Eval::use_simple_eval(int Eval) { return std::abs(Eval) > 1731; }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
 // of the position from the point of view of the side to move.
@@ -58,7 +60,11 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     assert(!pos.checkers());
 
-    bool smallNet           = use_smallnet(pos);
+    int Eval = simple_eval(pos);
+
+    if (use_simple_eval(Eval)) return Eval;
+
+    bool smallNet           = use_smallnet(Eval);
     auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, accumulators, &caches.small)
                                        : networks.big.evaluate(pos, accumulators, &caches.big);
 
--- src/evaluate.h
+++ src/evaluate.h
@@ -45,7 +45,8 @@ class AccumulatorStack;
 std::string trace(Position& pos, const Eval::NNUE::Networks& networks);
 
 int   simple_eval(const Position& pos);
-bool  use_smallnet(const Position& pos);
+bool  use_smallnet(int Eval);
+bool  use_simple_eval(int Eval);
 Value evaluate(const NNUE::Networks&          networks,
                const Position&                pos,
                Eval::NNUE::AccumulatorStack&  accumulators,
